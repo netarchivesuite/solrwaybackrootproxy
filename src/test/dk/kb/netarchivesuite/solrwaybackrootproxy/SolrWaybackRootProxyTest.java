ca147f63d21e0e7d015919161da115d1019fdd9c
teg <teg@kb.dk>
2020-09-23 09:57:34 +0200

    unittest (empty) so far

--- src/test/dk/kb/netarchivesuite/solrwaybackrootproxy/SolrWaybackRootProxyTest.java
+++ src/test/dk/kb/netarchivesuite/solrwaybackrootproxy/SolrWaybackRootProxyTest.java
@@ -2,8 +2,15 @@
 public class SolrWaybackRootProxyTest {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        
+        
+        String leakUrl="http://teg-legion-y540-15irh:8080/images/footer_gfx.png";
+        
+        String fullrefererUrl="http://teg-legion-y540-15irh:8080/solrwayback/services/webProxy/20200921122306/http://www.denoffentlige.dk/sites/all/themes/denoffentlige/stylesheet/mainstyle.full.min.css?v=111218a";
 
+        
+
+        
     }
 
 }
